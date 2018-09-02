17276ab5d65e1f989d737092842473a7771c557a
Ardon Bailey <waystylez1@gmail.com>
2018-09-01 17:23:41 -0700

    remove some stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,38 @@
 module.exports = {
+  siteMetadata: {
+    title: 'Gatsby + Netlify CMS Starter'
+  },
   plugins: [
-    `gatsby-plugin-netlify-cms`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/pages/blog`,
-        name: 'markdown-pages'
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
       }
     },
-    `gatsby-transformer-remark`
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/img`,
+        name: 'images'
+      }
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: []
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
+    },
+    'gatsby-plugin-netlify' // make sure to keep it last in the array
   ]
 };
