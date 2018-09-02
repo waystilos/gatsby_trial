353bbf4f199743208bff774fe273283d2e531b5b
Ardon Bailey <waystylez1@gmail.com>
2018-09-01 17:13:58 -0700

    change up path

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/src/pages/blog`,
         name: 'markdown-pages'
       }
     },
